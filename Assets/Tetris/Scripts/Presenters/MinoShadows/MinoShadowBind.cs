867aa6aec1fcdcbaaadb2614d4e7d161e529f729
toppy777 <toppy01257@yahoo.co.jp>
2022-09-21 08:15:38 +0900

    重複している名前空間の指定を消した

--- Assets/Tetris/Scripts/Presenters/MinoShadows/MinoShadowBind.cs
+++ Assets/Tetris/Scripts/Presenters/MinoShadows/MinoShadowBind.cs
@@ -2,7 +2,6 @@ using UnityEngine;
 using System.Collections.Generic;
 using System;
 using UniRx;
-using System;
 using Tetris.Scripts.Domains.MinoShadows;
 using Tetris.Scripts.Presenters.MinoPieces;
 
