44cdffc9992e919982079a906d60c774fc6e5ed0
toppy777 <toppy01257@yahoo.co.jp>
2022-09-30 07:37:37 +0900

    インターバルでミノが置かれた場合の音を追加

--- Assets/Tetris/Scripts/Applications/Minos/PlaceMinoUseCase.cs
+++ Assets/Tetris/Scripts/Applications/Minos/PlaceMinoUseCase.cs
@@ -1,22 +1,31 @@
+using UnityEngine;
 using Tetris.Scripts.Domains.Games;
+using Tetris.Scripts.Domains.Audios;
+using Tetris.Scripts.Infrastructures.Sounds;
 
 namespace Tetris.Scripts.Application.Minos
 {
     public class PlaceMinoUseCase
     {
         GameRegistry _gameRegistry;
+        IAudio _audio;
+        AudioClip _audioClip;
 
         public PlaceMinoUseCase(
-            GameRegistry gameRegistry
+            GameRegistry gameRegistry,
+            IAudio audio
         )
         {
             _gameRegistry = gameRegistry;
+            _audio = audio;
+            _audioClip = SoundRepository.GetSoundPlaceMino();
         }
 
         public void Execute()
         {
             Game game = _gameRegistry.CurrentGame;
             game.Board.Add(game.Mino);
+            _audio.PlaySound(_audioClip);
             game.Mino.Release();
             game.HorizontalPosition.Reset();
         }
